d8a54fe4fb19212eab4b359eae84d7d35d2a3ac7
Vincent Widjaya <vwidjaya@uw.edu>
2020-04-06 06:32:11 -0700

    disable PWA unless prod env

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withPWA = require('next-pwa');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = withPWA({
   publicRuntimeConfig: {
     BE_ADDR: process.env.BE_ADDR,
     FE_ADDR: process.env.FE_ADDR
   },
   pwa: {
+    disable: !isProd,
     dest: 'public'
   }
 });
