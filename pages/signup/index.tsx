a821fd49249a0ff631d6eb789d4f16baae31ee5e
Vincent Widjaya <vwidjaya@uw.edu>
2020-05-01 22:30:48 -0700

    revamp signup page

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,15 +1,97 @@
 import React, { useState } from 'react';
 import { NextPage } from 'next';
 import Link from 'next/link';
-import Router from 'next/router';
-import { Avatar, Button, TextField, Typography } from '@material-ui/core';
-import { Grid, Container, Paper } from '@material-ui/core';
+import { Button, TextField, Typography, InputAdornment, IconButton } from '@material-ui/core';
+import { Box, Container } from '@material-ui/core';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
 import { Alert } from '@material-ui/lab';
+import { makeStyles } from '@material-ui/core/styles';
 import fetch from 'isomorphic-unfetch';
 
-import CopyrightFooter from 'components/CopyrightFooter';
-import useStyles from 'public/static/styles/auth';
 import { BE_ADDR, checkAuth, redirectPage } from 'utils';
+
+const useStyles = makeStyles((theme) => ({
+  outer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    minHeight: 'calc(100% - 6rem)',
+    marginTop: '2rem',
+    marginBottom: 0,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  button: {
+    border: '2px solid #F05A28',
+    borderRadius: '10px',
+    width: '100%',
+    color: theme.palette.primary.main,
+    fontWeight: 'bold',
+    fontSize: theme.spacing(2.5),
+    '&:hover': {
+      backgroundColor: theme.palette.primary.main,
+      color: 'white',
+    },
+  },
+  heading: {
+    fontSize: theme.spacing(3),
+  },
+  text: {
+    fontSize: theme.spacing(2.25),
+  },
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.primary.main,
+  },
+  skip: {
+    textDecoration: 'none',
+    fontWeight: 'bold',
+    color: '#BBBBBB',
+    fontSize: theme.spacing(2.5),
+  },
+}));
+
+const SuccessPage: NextPage<{ firstName: string }> = ({ firstName }) => {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs" className={classes.outer}>
+      <Box marginTop="6rem">
+        <Typography component="h1" align="center" className={classes.heading}>
+          Welcome <span style={{ fontWeight: 'bold' }}>{firstName},</span>
+        </Typography>
+      </Box>
+
+      <Box marginBottom="14rem">
+        <Typography align="center" className={classes.text}>
+          For your better experience with searching projects and team members,
+        </Typography>
+        <Typography align="center" style={{ marginTop: '.5rem' }}>
+          <Link href="/profile">
+            <a className={`${classes.link} ${classes.text}`}>please set your preferences.</a>
+          </Link>
+        </Typography>
+      </Box>
+
+      <Box width="60%">
+        <Link href="/profile">
+          <Button className={classes.button} aria-label="Join" size="large">
+            Set Up Profile
+          </Button>
+        </Link>
+      </Box>
+
+      <Box textAlign="right" width="100%" paddingRight="1rem">
+        <Link href="/">
+          <a className={classes.skip}>Skip</a>
+        </Link>
+      </Box>
+    </Container>
+  );
+};
 
 const SignupPage: NextPage = () => {
   const classes = useStyles();
@@ -18,14 +100,15 @@ const SignupPage: NextPage = () => {
   const [lastName, setLastName] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-  const [success, setSuccess] = useState(true);
-  const userType = 'Student';
+  const [success, setSuccess] = useState(null);
 
   const handleSignup = async (event): Promise<void> => {
     event.preventDefault();
 
     try {
+      const userType = 'Student';
       const signupRes = await fetch(`${BE_ADDR}/auth/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -45,109 +128,95 @@ const SignupPage: NextPage = () => {
       if (!studentRes.ok) throw new Error('Failed to create student profile');
 
       setSuccess(true);
-      Router.push('/signup/profile');
     } catch (err) {
       setSuccess(false);
     }
   };
 
+  if (success !== null && success) return <SuccessPage firstName={firstName} />;
+
   return (
-    <>
-      <Container component="main" maxWidth="xs" className={classes.outer}>
-        <Paper elevation={2} className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <p>N</p>
-          </Avatar>
-          <Typography component="h1" variant="h5">
+    <Container component="main" maxWidth="xs" className={classes.outer} style={{ justifyContent: 'center' }}>
+      <form className={classes.form} onSubmit={handleSignup}>
+        <Typography style={{ fontWeight: 'bold' }}>Sign Up</Typography>
+
+        <TextField
+          variant="outlined"
+          margin="normal"
+          label="First Name"
+          id="first-name"
+          name="first-name"
+          autoComplete="given-name"
+          required
+          fullWidth
+          autoFocus
+          onChange={(e): void => setFirstName(e.target.value)}
+        />
+
+        <TextField
+          variant="outlined"
+          margin="normal"
+          label="Last Name"
+          id="last-name"
+          name="last-name"
+          autoComplete="family-name"
+          required
+          fullWidth
+          onChange={(e): void => setLastName(e.target.value)}
+        />
+
+        <TextField
+          variant="outlined"
+          margin="normal"
+          label="Email Address"
+          id="email"
+          name="email"
+          type="email"
+          autoComplete="email"
+          required
+          fullWidth
+          onChange={(e): void => setUsername(e.target.value)}
+        />
+
+        <TextField
+          variant="outlined"
+          margin="normal"
+          name="password"
+          id="password"
+          label="Password"
+          autoComplete="curent-password"
+          required
+          fullWidth
+          type={showPassword ? 'text' : 'password'}
+          onChange={(e): void => setPassword(e.target.value)}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={(): void => setShowPassword(!showPassword)}
+                  onMouseDown={(e): void => e.preventDefault()}
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        {success !== null && !success ? (
+          <Box marginTop="1rem">
+            <Alert severity="error">Someone's already using that email.</Alert>
+          </Box>
+        ) : null}
+
+        <Box marginX="20%" marginY="3rem">
+          <Button type="submit" aria-label="Sign Up" size="large" className={classes.button}>
             Sign Up
-          </Typography>
-
-          <form className={classes.form} style={{ marginTop: '1.5rem' }} onSubmit={handleSignup}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  variant="outlined"
-                  label="First Name"
-                  id="first-name"
-                  name="first-name"
-                  autoComplete="given-name"
-                  required
-                  fullWidth
-                  autoFocus
-                  onChange={(e): void => setFirstName(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  variant="outlined"
-                  label="Last Name"
-                  id="last-name"
-                  name="last-name"
-                  autoComplete="family-name"
-                  required
-                  fullWidth
-                  onChange={(e): void => setLastName(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  label="Email Address"
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  fullWidth
-                  onChange={(e): void => setUsername(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  label="Password"
-                  name="password"
-                  type="password"
-                  id="password"
-                  autoComplete="curent-password"
-                  required
-                  fullWidth
-                  onChange={(e): void => setPassword(e.target.value)}
-                />
-              </Grid>
-            </Grid>
-
-            {!success ? (
-              <Alert className={classes.alert} severity="error">
-                Someone's already using that email.
-              </Alert>
-            ) : null}
-
-            <Button
-              type="submit"
-              aria-label="Sign Up"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign Up
-            </Button>
-          </form>
-        </Paper>
-
-        <Paper elevation={2} className={classes.paper}>
-          <Typography variant="body2" align="center">
-            {`Have an account? `}
-            <Link href="/login">
-              <a className={classes.link}>Log In</a>
-            </Link>
-          </Typography>
-        </Paper>
-      </Container>
-
-      <CopyrightFooter />
-    </>
+          </Button>
+        </Box>
+      </form>
+    </Container>
   );
 };
 
